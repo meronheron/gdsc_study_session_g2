e72d275a9977e59d67975f650e9866e4504af76d
Meron Getaneh <merongetaneh.sel9@gmail.com>
2023-12-19 23:56:32 +0300

    script

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,16 @@
+let result = document.getElementById('result');
+function AddToResult(value) {
+    result.value += value;
+}
+function clear() {
+    result.value = '';
+}
+function calculate() {
+    try {
+    result.value = eval(result.value);
+    } catch (error) {
+      result.value = 'Error';
+    }
+  }
+
 
